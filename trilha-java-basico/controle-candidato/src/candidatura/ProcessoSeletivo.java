d2ad101fe80b2d0dc1431f4bba9408ecb178237e
[EXT] - RAPHAEL DA COSTA PERET <rperet@brasilseg.com.br>
2025-04-03 20:59:17 -0300

    Atualização de impressão e seleção de canidatos

--- trilha-java-basico/controle-candidato/src/candidatura/ProcessoSeletivo.java
+++ trilha-java-basico/controle-candidato/src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,47 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
-		
-		analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(2000.0);
+		imprimirSelecionados();
 
+	}
+	
+	static void imprimirSelecionados() {
+		String [] candidatos = {"FELIPE", "MARCIA", "JUIA", "PAULO", "AUGUSTO", "MONICA"};
+		
+		System.out.println("Imprimindo a lista de candidatos:");
+		
+		for(String candidato : candidatos) {
+			System.out.println(candidato);
+		}
+	}
+	
+	static void selecaoDeCandidatos() {
+		String [] candidatos = {"FELIPE", "MARCIA", "JUIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELLA", "DANIELA", "JORGE"};
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou o salário de " + salarioPretendido);
+			
+			if(salarioBase > salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+				candidatosSelecionados++;
+			}
+			
+			candidatoAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);	
 	}
 	
 	static void analisarCandidato(double salarioPretendido) {
